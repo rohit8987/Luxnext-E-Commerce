794190f264c5400a18f5bbaf9084d5c1ad735e17
Alone <alone@localhost.localdomain>
2025-01-06 19:25:36 +0530

    product Details

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -2,46 +2,53 @@ import { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom"
 import { ShopContext } from "../Components/ShopContext";
 import StarIcon from '@mui/icons-material/Star';
+import { doc, getDoc, setDoc, Timestamp } from "firebase/firestore";
 import StarHalfIcon from '@mui/icons-material/StarHalf';
-import RelatedProducts from "../Components/RelatedProducts";
+// import { fireDB } from "../../Firebase/Firebaseconfig";
+import Relatedproducts from "../Components/RelatedProducts"
+import { fireDB } from "../Firebase/Firebaseconfig";
 
 const Product = () => {
 
-  const { productId } = useParams();
-  const { products, currency, addToCart } = useContext(ShopContext);
-  const [productData, setProductData] = useState(false);
+  const { id } = useParams();
+  // const { product, currency, addToCart } = useContext(ShopContext);
+  const [productData, setProductData] = useState({});
   const [image, setImage] = useState(' ')
   const [size, setSize] = useState('')
-
+  // const context = useContext(MyContext);
+    // const { loading, getAllProduct } = context;
   
 
 
-  const fetchProductData = async () => {
+    
+  // const fetchProductData = async () => {
 
-    products.map((item) => {
-      if (item._id === productId) {
-        setProductData(item)
-        setImage(item.image[0])
-        return null;
-      }
-    })
-  }
+  //   product.map((item) => {
+  //     if (item._id === id) {
+  //       setProductData(item)
+  //       setImage(item.image[0])
+  //       return null;
+  //     }
+  //   })
+  // }
+
+  console.log(productData, "product", id)
 
   useEffect(() => {
-    fetchProductData();
-
-  }, [productId])
+    const fetch_data = async () => {
+      const productTemp = await getDoc(doc(fireDB, "product", id));
+      const product_data = productTemp.data();
+      setProductData(product_data)
+    }
+    fetch_data();
+  }, [id])
 
   return productData ? (
     <div className="border-t-2 pt-10 transition-opacity ease-in duration-500 opacity-100">
       <div className="flex gap-12 sm:gap-12 flex-col sm:flex-row">
         <div className="flex-1 flex flex-col-reverse gap-3 sm:flex-row">
           <div className="flex sm:flex-col overflow-x-auto sm:overflow-y-scroll justify-between sm:justify-normal sm:w-[18.7%] w-full">
-            {
-              productData.image.map((item, index) => (
-                <img onClick={() => setImage(item)} src={item} key={index} className="w-[24%] sm:w-full sm:mb-3 flex-shrink-0 cursor-pointer" alt="" />
-              ))
-            }
+              <img onClick={() => setImage(item)} src={productData.productImageUrl} className="w-[24%] sm:w-full sm:mb-3 flex-shrink-0 cursor-pointer" alt="" />
           </div>
           <div className="w-full sm:w-[80%]">
             <img className="w-full h-auto " src={image} alt="" />
@@ -57,14 +64,14 @@ const Product = () => {
             <StarHalfIcon />
             <p className="pl-2 text-black">(121)</p>
           </div>
-          <p className="mt-5 text-3xl font-medium">{currency}{productData.price}</p>
+          <p className="mt-5 text-3xl font-medium">{productData.price}</p>
           <p className="mt-5 text-gray-500 md:w-4/5">{productData.description}</p>
           <div className="flex flex-col gap-4 my-8">
             <p className="">Select Size</p>
             <div className="flex gap-2">
-              {productData.sizes.map((item, index) => (
+              {/* {productData.sizes.map((item, index) => (
                 <button onClick={() => setSize(item)} className={`border py-2 px-4 bg-gray-100 ${item === size ? 'border-gray-700' : ''}`} key={index} > {item} </button>
-              ))}
+              ))} */}
             </div>
           </div>
           <button onClick={()=>addToCart(productData._id,size)} className="uppercase bg-black text-white px-8 py-3 text-sm active:bg-gray-600">Add to cart</button>
@@ -87,10 +94,10 @@ const Product = () => {
         </div>
       </div>
 
-      <RelatedProducts category={productData.category} subCategory={productData.subCategory}/>
+      <Relatedproducts category={productData.category} subCategory={productData.subCategory}/>
     </div>
-  ) : <div className="opacity-0 ">
-
+  ) : <div className="">
+    "waiter"
   </div>
 }
 
